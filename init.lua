c5e8880668dbf769bfc8b43bd83d456e8b6043a8
Mathis-Werner <wer.mathis@gmail.com>
2025-09-30 18:35:25 +0200

    first commit

---
+++ init.lua
@@ -0,0 +1,36 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader= " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+vim.lsp.enable({'clangd','lua_ls'})
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
+})
+
+vim.cmd("set completeopt+=noselect")
+
+vim.o.winborder = 'rounded'
+
+local opts = {}
+
+require("lazy").setup("plugins")
+
