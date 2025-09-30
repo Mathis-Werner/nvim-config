0a52c33298f8e4a1cf5739d0cceab1f8145ca2d4
Mathis-Werner <wer.mathis@gmail.com>
2025-09-30 22:16:03 +0200

    Started cleanup of init

---
+++ lua/config/vim-conf.lua
@@ -0,0 +1,19 @@
+-- options
+---------------------------------------------
+
+--- Tabstops
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- Set leaderkey
+vim.g.mapleader= " "
+
+-- Realitve and absolute line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
