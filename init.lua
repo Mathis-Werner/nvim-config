26191fba5141a0eeee8ab6bcb51dc212a8886b4b
Mathis-Werner <wer.mathis@gmail.com>
2025-09-30 22:23:39 +0200

    moved LSP inti to seperat config

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require('config.vim-conf')
+require('config.lsp')
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -11,22 +12,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 		lazypath,
 	})
 end
+
 vim.opt.rtp:prepend(lazypath)
-vim.lsp.enable({'clangd','lua_ls'})
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-    end
-  end,
-})
-
-vim.cmd("set completeopt+=noselect")
-
-vim.o.winborder = 'rounded'
-
-local opts = {}
 
 require("lazy").setup("plugins")
 
--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -2,4 +2,15 @@ return {
   cmd = { 'lua-language-server' },
   root_markers = { '.luarc.json', '.luarc.jsonc', '.git' },
   filetypes = { 'lua' },
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+    },
+  },
 }
